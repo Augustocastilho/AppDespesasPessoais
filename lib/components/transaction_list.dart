34f80456d4273cdbd2bd9d0fd78b8885bd0f6a03
Augusto Castilho <castilho@ice.ufjf.br>
2020-04-21 20:03:42 -0300

    upload do app no git

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:expanses/models/transaction.dart';
+import 'transaction_item.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  TransactionList(this.transactions, this.onRemove);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                SizedBox(height: constraints.maxHeight * 0.03),
+                Text(
+                  'Nenhuma transação cadastrada',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(height: constraints.maxHeight * 0.03),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          })
+        : ListView(
+            children: transactions.map((tr) {
+              return TransactionItem(
+                key: ValueKey(tr.id),
+                tr: tr,
+                onRemove: onRemove,
+              );
+            }).toList(),
+          );
+    /*ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return TransactionItem(
+                key: GlobalObjectKey(tr),
+                tr: tr,
+                onRemove: onRemove,
+              );
+            },
+          );*/
+  }
+}
